ad85bd7fcf9082af1bb5131dcbc69e3a7939926a
neilxstc <neilmxstc@gmail.com>
2020-07-26 13:56:34 +0300

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    private float cameraOffset = 2;
+    private Vector3 targetPosition;
+    private readonly float smoothing = 1 ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+        if (target.transform.localScale.y > 0f)
+        {
+            targetPosition = new Vector3(targetPosition.x, targetPosition.y + cameraOffset, targetPosition.z);
+        }
+        else {
+            targetPosition = new Vector3(targetPosition.x , targetPosition.y - cameraOffset, targetPosition.z);
+        }
+       // transform.position = targetPosition;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime); // how long it taked from one frame to another (fps).
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+
+
+    private float moveSpeed;
+    public Rigidbody2D rigidbody2D;
+
+    // bound stuff
+    public Transform boundCheck;
+    public float boundCheckRadius;
+    public LayerMask whatIsBound;
+    public bool boundHit;
+
+    public Vector3 responPosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 10;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        responPosition = transform.position;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+        //// player Movmement;
+        //if (Input.GetAxisRaw("Horizontal") > 0f)
+        //{
+        //    rigidbody2D.velocity = new Vector3(moveSpeed, rigidbody2D.velocity.y, 0f);
+        //    transform.localScale = new Vector3(1f, 1f, 1f);
+
+        //}
+        //else if (Input.GetAxisRaw("Horizontal") < 0f)
+        //{
+        //    rigidbody2D.velocity = new Vector3(-moveSpeed, rigidbody2D.velocity.y, 0f);
+        //    transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        //}
+        //else {
+        //    rigidbody2D.velocity = new Vector3(0f, rigidbody2D.velocity.y, 0f);
+        //}
+    }
+
+    private void Movement() {
+        // player Movmement;
+
+        
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rigidbody2D.velocity = new Vector3(moveSpeed, rigidbody2D.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            rigidbody2D.velocity = new Vector3(-moveSpeed, rigidbody2D.velocity.y, 0f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector3(0f, rigidbody2D.velocity.y, 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, moveSpeed, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, -moveSpeed, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else {
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, 0f, 0f);
+            transform.localScale = new Vector3 (1f, 1f, 1f);
+        }
+
+    }
+}
